7c08e96c0510e947198b11afc7408da2fd4103db
Ekaterina Galiakhmetova <kgaliakhmetove@gmail.com>
2021-08-17 14:27:03 +0300

    Неработающая API, ошибка 403

--- src/components/Content_list/Content.tsx
+++ src/components/Content_list/Content.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { sendRequest } from '../Request';
+import React, { useState, useEffect } from 'react';
+// import { resolvers} from '../Request';
 import classes from './Content_list.module.css'
 
 interface IContentProps {
@@ -9,20 +9,30 @@ interface IContentProps {
 const Content: React.FC <IContentProps>  = ({category}) => {
     const [films, setFilms] = useState<any[]>([]);
     const [loaded, setLoaded] = useState<boolean>(false);
-    const requestURL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1'
-        if (category === "Movie") {
-    sendRequest('GET', requestURL)
-        .then(response => {
-            setFilms(response.films);
-            setLoaded(true);
-        })
-        .catch(error => {
-            console.log(
-              "error",
-              error
-            );
-        }); 
+    const headers = {
+        'accept' : 'application/json', 
+        'X-API-KEY' : '12c15430-9199-4e20-b554-373d3d40df43'
     }
+    fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+            query: `
+            query {
+                pagesCount
+                films {
+                    posterUrl
+                }
+            }
+            `
+        })
+    })
+    .then (res => res.json())
+    .then (data => {
+        console.log(data.data)
+    })
+    
+         
     return(
         <div>
             <ul>
