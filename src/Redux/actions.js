a9b7b90afabde286c91f5d6692d1e1b42c17319e
Ekaterina Galiakhmetova <kgaliakhmetove@gmail.com>
2021-09-20 20:16:52 +0300

    самогенерирующиеся файлы js, удаление

--- src/Redux/actions.js
+++
@@ -1,8 +0,0 @@
-export var addNote = function (note) { return ({
-    type: "ADD_NOTE",
-    payload: note
-}); };
-export var addReview = function (review) { return ({
-    type: "ADD_REVIEW",
-    payload: review
-}); };
